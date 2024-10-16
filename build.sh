75d421ec194481de91f7253cbecf103c8b8d29ae
Zap <67962871+ziap@users.noreply.github.com>
2024-10-16 09:49:08 +0700

    Strip more symbols

--- build.sh
+++ build.sh
@@ -3,14 +3,10 @@
 CC="${CC:-clang}"
 
 CFLAGS="-std=c99 -Wall -Wextra -pedantic -fshort-enums"
-LDLIBS=""
-
-BUILD_FLAGS="-O3 -march=native -mtune=native -s"
-DEBUG_FLAGS="-Og -ggdb"
 
 WASM_CFLAGS="-O3 --target=wasm32 -flto -nostdlib -fvisibility=hidden"
 WASM_CFLAGS+=" -mbulk-memory -msimd128"
-WASM_LDFLAGS="--no-entry --strip-debug --lto-O3 --allow-undefined --export-dynamic"
+WASM_LDFLAGS="--no-entry --strip-all --lto-O3 --allow-undefined --export-dynamic"
 
 WASM_FLAGS="$WASM_CFLAGS"
 for FLAG in $WASM_LDFLAGS
@@ -25,5 +21,5 @@ SRCS="$(ls src/*.c)"
 for SRC in $(ls src/wasm/*.c)
 do
   OUTPUT="wasm/$(basename ${SRC%.*}).wasm"
-  $CC $CFLAGS $LDLIBS $WASM_FLAGS -o $OUTPUT $SRC $SRCS
+  $CC $CFLAGS $WASM_FLAGS -o $OUTPUT $SRC $SRCS
 done
